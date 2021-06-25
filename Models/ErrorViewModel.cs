950c6c48addaea05c21589c450ff7f6cc37a5ec7
GustavoBorges <sgustavo.borges10@gmail.com>
2021-06-25 10:41:26 -0300

    Subindo pasta para repositorio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjetoInstaDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
